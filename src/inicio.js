cf01daed880e6bbd2451065431a3d4608859f0dd
Raimundo <raimundo_nn16@hotmail.com>
2024-03-05 11:24:53 -0300

    coomit inicial

---
+++ src/inicio.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="menu">
+        <h2>RN Consultoria</h2>
+          <div className="selec">
+            <input type="text" placeholder="Usuário"/>
+            <input type="password" placeholder="Senha"/>
+            <button type="submit">Entrar</button>
+            <button type="submit">Criar Cadastro</button>
+            <p>Esqueci minha senha</p>
+          </div>  
+      </div>
+    </div>
+  );
+}
+
+export default App;
